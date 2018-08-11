140fec3c93276d7d7dfafa71a83eb3be0fc003c4
WarsFeng <huangrongfeng1999@gmail.com>
2018-08-11 20:52:18 +0800

    Init file

---
+++ src/cat/wars/Main.java
@@ -0,0 +1,16 @@
+package cat.wars;
+
+/**
+ * \* Created with IntelliJ IDEA.
+ * \* User: wars
+ * \* Date: 18-8-11
+ * \* Time: 下午8:14
+ * \* Description:
+ * \
+ */
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
